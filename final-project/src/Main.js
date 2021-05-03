0f1dcc8a8bb63425fc0192499eba4d5c5084de96
Anish Ganga <anishreddy@gmail.com>
2021-05-03 17:05:07 -0400

    merged main app with login

--- final-project/src/Main.js
+++ final-project/src/Main.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
+import Hero from './components/Hero';
+import Products from './components/Products';
+import { productData, productDataTwo } from './components/Products/data';
+import Feature from './components/Feature';
+import Footer from './components/Footer';
 
 const Main = ({logoutHandler, getData}) => {
     return(
         <div>
             <h1>Hello There</h1>
-            <button onClick = {getData}>Data</button>
+                <button onClick = {getData}>Data</button>
             <button onClick = {logoutHandler}>Log Out</button>
-        </div>
+        <Router>
+        <GlobalStyle />
+        <Hero />
+        <Products heading='Pick Your Sweet Ride' data={productData}/>
+        <Feature />
+        <Products heading='Grab a Parking Spot' data={productDataTwo} />
+        <Footer/>
+        </Router>
+      </div>
     );
 };
 
