dc74b4ab773ae2cce643fa386628f6f5463d3ff5
Anish Ganga <anishreddy@gmail.com>
2021-05-03 03:26:10 -0400

    added authentication

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Login from './Login.js' 
+import fire from './fire.js';
+
 
 function App() {
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [hasAccount, setHasAccount] = useState('false');
+
+  const loginHandler = () =>  {
+    fire
+        .auth()
+        .signInWithEmailAndPassword
+        .catch((err) => {
+            alert(err.message);
+        });
+  }
+
+  const signUpHandler = () => {
+    
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+          alert(err.message);
+      });
+  }
+
+  const logoutHandler = () => {
+    fire.auth().signOut();
+  }
+
+  const authListener = () => {
+    fire.auth().onAuthStateChanged((user) => {
+      if(user) {
+        clearValues();
+        setUser(user);
+      } else {
+        setUser('');
+      }
+    })
+
+  }
+  
+  useEffect(() => {
+    authListener();
+  }, []);
+
+  const clearValues = () => {
+    setEmail('');
+    setPassword('');
+  }
+    
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Login 
+      email = {email}
+      setEmail = {setEmail}
+      password = {password}
+      setPassword = {setPassword}
+      loginHandler = {loginHandler}
+      hasAccount = {hasAccount}
+      setHasAccount = {setHasAccount}
+      signUpHandler = {signUpHandler}
+    />
   );
 }
 
---
+++ final-project/src/fire.js
@@ -0,0 +1,15 @@
+import Firebase from 'firebase'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDQ9fBNst3TX337YQI0hV4CSfaUex9NHAM",
+    authDomain: "authapp-2e34c.firebaseapp.com",
+    projectId: "authapp-2e34c",
+    storageBucket: "authapp-2e34c.appspot.com",
+    messagingSenderId: "31535714740",
+    appId: "1:31535714740:web:a1cb29a2c2dfb73bd9e2e1",
+    measurementId: "G-N9LQWT848S"
+  };
+
+  const fire = Firebase.initializeApp(firebaseConfig);
+
+  export default fire;
