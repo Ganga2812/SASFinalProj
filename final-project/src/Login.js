c595fa9117e867b1fc36118fd683606aacf93e4f
Saad Mahmood <saadsmahmood@gmail.com>
2021-05-04 06:58:42 -0400

    Css for login

--- final-project/src/Login.js
+++ final-project/src/Login.js
@@ -19,30 +19,30 @@ class Login extends Component {
     render() {
 
     return(   
-        <>
+        <div class="login">
             <h2>Sign-in Page</h2>
             <div>
-                <label>Email Address</label>
-                <input type = "text" value ={this.props.email} onChange = {(e) => this.props.setEmail(e.target.value)}></input>
+                <label>Email Address </label>
+                <input type = "text" value ={this.props.email} onChange = {(e) => this.props.setEmail(e.target.value)} placeholder="Email Address"></input>
             </div>
             <div>
-                <label>Password</label>
-                <input type = "password" value ={this.props.password} onChange = {(e) => this.props.setPassword(e.target.value)}></input>
+                <label>Password </label>
+                <input type = "password" value ={this.props.password} onChange = {(e) => this.props.setPassword(e.target.value)} placeholder="Password"></input>
             
             </div>
             <br/>
             {this.props.hasAccount ? (
                 <div>
-                    <button onClick={this.props.loginHandler}>Login</button>
-                    <p>Don't have an account? <span><a onClick = {() => this.props.setHasAccount(!this.props.hasAccount)}>Create one.</a></span></p>
+                    <button class="loginButton" onClick={this.props.loginHandler}>Login</button>
+                    <p>Don't have an account? <span class="linkBox"><a onClick = {() => this.props.setHasAccount(!this.props.hasAccount)}>Create one.</a></span></p>
                 </div>
             ):(
                 <div>
-                    <button onClick={this.props.signUpHandler}>Sign up</button>
-                    <p>Have an account? <span><a onClick = {() => this.props.setHasAccount(!this.props.hasAccount)}>Login.</a></span></p>
+                    <button class="loginButton" onClick={this.props.signUpHandler}>Sign up</button>
+                    <p>Have an account? <span class="linkBox"><a onClick = {() => this.props.setHasAccount(!this.props.hasAccount)}>Login.</a></span></p>
                 </div>
             )}
-       </>
+       </div>
     );
     }
     
