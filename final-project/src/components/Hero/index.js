abcc9cb0a414ed68943bca6cc3b18d3264e09d16
Saadh Ahmed <44828093+saadhahmed00@users.noreply.github.com>
2021-05-04 06:39:48 -0400

    welcome admin

--- final-project/src/components/Hero/index.js
+++ final-project/src/components/Hero/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Navbar from '../Navbar';
 import Sidebar from '../Sidebar';
+import fire from '../../fire.js';
 import {
   HeroContainer,
   HeroContent,
@@ -17,6 +18,22 @@ const Hero = ({check, setCheck}) => {
     setIsOpen(!isOpen);
   };
 
+  if(fire.auth().currentUser.uid === '4O2b4p4UNbXisjCdOXX7k0l0uKk1'){
+    return (
+      <HeroContainer>
+        <Navbar toggle={toggle} />
+        <Sidebar isOpen={isOpen} toggle={toggle} check={check} setCheck={setCheck}/>
+        <HeroContent>
+          <HeroItems>
+            <HeroP>Welcome Admin!</HeroP>
+            <HeroH1>Best Car Rental Service in the World</HeroH1>
+            <HeroP>Sign and Drive in 5 minutes</HeroP>
+            <HeroBtn smooth to='#SUV'>Rent a Car Today</HeroBtn>
+          </HeroItems>
+        </HeroContent>
+      </HeroContainer>
+    );
+  } else{
   return (
     <HeroContainer>
       <Navbar toggle={toggle} />
@@ -30,6 +47,7 @@ const Hero = ({check, setCheck}) => {
       </HeroContent>
     </HeroContainer>
   );
+  }
 };
 
 export default Hero;
